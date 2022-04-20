821a9db077f579712fb19cbbad948649e2beb6b7
quacke69 <79087895+quacke69@users.noreply.github.com>
2022-04-20 14:18:44 +0200

    Lade till kodkommentarer

--- ATtiny85 code/code_v0.1.ino
+++ ATtiny85 code/code_v0.1.ino
@@ -1,3 +1,10 @@
+// HAS NOT BEEN TESTED
+// Experimental code, not tested on new ATtiny85 microcontroller.
+// Based on code used with ESP32. 
+
+// NOTE: In later versions of the code, if-statement on line 48
+// may be removed entirely, for power saving reasons.
+
 #include <BH1750.h>
 #include <RH_ASK.h>
 #include <SPI.h>
@@ -8,48 +15,49 @@
 #include <avr/wdt.h>
 #define TIME_TO_SLEEP 5000000
 
-BH1750 lightMeter;
-RH_ASK driver(2000, 11, 25, 0);
-RTC_DATA_ATTR unsigned short int old_lux = 0;
+BH1750 lightMeter; // creation of light-sensor device
+RH_ASK driver(2000, 11, 25, 0); // creation of RF driver
+RTC_DATA_ATTR unsigned short int old_lux = 0; 
 
 const byte txPin = 3;
 const byte sdaPin = 5;
-const byte sclPin = 7;
+const byte sclPin = 7; // init of pins
 byte saveADCSRA;
 volatile byte counterWD = 0;
 
 void setup()
 {
     Serial.begin(4800);
-    if (!driver.init())
+    if (!driver.init()) // init of RF driver
         Serial.println("init failed");
     resetWatchDog();
-    pinMode(5, sdaPin);  // SDA
-    pinMode(7, sclPin); // SCL
+    pinMode(5, sdaPin);  // SDA for sensor
+    pinMode(7, sclPin); // SCL for sensor
     pinMode(3, txPin); // Transmitter
     Wire.begin(33, 32, 25);
 
-    lightMeter.begin();
+    lightMeter.begin(); // init of light sensor
 }
 
 void loop()
 {
     driver.setModeTx();
     // esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP);
-    unsigned short int lux = lightMeter.readLightLevel();
-    unsigned short int *msg;
+    unsigned short int lux = lightMeter.readLightLevel(); // read lux from sensor
+    unsigned short int *msg; // msg to be transmitted
     if (lux / old_lux > 1.03 || lux / old_lux < 0.97 && lux > 50)
     {
         old_lux = lux;
-        msg = &lux;
-        if (!driver.send((uint8_t *)msg, 2))
+        msg = &lux; // message points to lux value
+        if (!driver.send((uint8_t *)msg, 2)) // sending package
             Serial.println("send failed");
         driver.waitPacketSent();
     }
     // esp_deep_sleep_start();
-    sleepNow();
+    sleepNow(); // enter deep sleep
 }
 
+// Sleep function to conserve power during downtime. 
 void sleepNow(){
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     // save ADCSRA = ADCSRA;
@@ -69,6 +77,7 @@ void sleepNow(){
     // ADCSRA = saveADCSRA;
 }
 
+// Function to reset ATtiny85's watchdog timer used during deep sleep. 
 void resetWatchDog(){
     MCUSR = 0;
     WDTCR = bit (WDCE) | bit (WDE) | bit (WDIF);
